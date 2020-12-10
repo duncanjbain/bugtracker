96d273051a479db8ef5af6536355acb75931ab27
Duncan Bain <duncan@bain.io>
2020-12-10 20:48:51 +0000

    Add colors and media queries to theme

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import theme from "./ui/theme"
+import {GlobalStyle, theme } from "./ui/theme"
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ul>
+        <li>Test</li>
+      </ul>
     </ThemeProvider>
 
   );
--- packages/frontend/src/ui/theme.js
+++ packages/frontend/src/ui/theme.js
@@ -1,19 +1,45 @@
-const screenSize = {
-    sm: '320px',
-    md: '480px',
-    lg: '768px',
-    xl: '1024px',
-    "2xl": '1536px'
-  };
+import { createGlobalStyle } from "styled-components";
+import reset from "modern-css-reset";
 
-const theme = {
+const screenSize = {
+  sm: "320px",
+  md: "480px",
+  lg: "768px",
+  xl: "1024px",
+  "2xl": "1536px",
+};
+
+export const theme = {
   media: {
     sm: `min-width: ${screenSize.sm}`,
     md: `min-width: ${screenSize.md}`,
     lg: `min-width: ${screenSize.lg}`,
     xl: `min-width: ${screenSize.xl}`,
-    "2xl": `min-width: ${screenSize['2xl']}`,
+    "2xl": `min-width: ${screenSize["2xl"]}`,
   },
+  colors: {
+    white: 'hsl(0, 0%, 100%',
+    black: 'hsl(0, 0%, 4%)',
+    light: 'hsl(0, 0%, 96%)',
+    dark: 'hsl(0, 0%, 21%)',
+    primary: 'hsl(171, 100%, 41%)',
+    link: 'hsl(217, 71%, 53%)',
+    info: 'hsl(204, 86%, 53%)',
+    success: 'hsl(141, 53%, 53%)',
+    warning: 'hsl(48, 100%, 67%)',
+    danger: 'hsl(348, 100%, 61%)',
+
+  }
 };
 
-export default theme;
+export const GlobalStyle = createGlobalStyle`
+${reset}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+               Roboto, Oxygen-Sans, Ubuntu, Cantarell,
+               "Helvetica Neue", sans-serif;
+  font-size: clamp(100%, 1rem + 2vw, 20px);
+}
+
+`;
