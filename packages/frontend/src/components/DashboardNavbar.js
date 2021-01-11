92854ebb6f15334c98848406b4dae22579d57678
Duncan Bain <duncan@bain.io>
2021-01-11 22:25:01 +0000

    linting

--- packages/frontend/src/components/DashboardNavbar.js
+++ packages/frontend/src/components/DashboardNavbar.js
@@ -2,37 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NavContainer = styled.nav`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const NavLinks = styled.ul`
-display: flex;
-padding-left: 0;
+  display: flex;
+  padding-left: 0;
 `;
 
 const NavItem = styled.li`
-list-style: none;
-padding: 0.5rem;
+  list-style: none;
+  padding: 0.5rem;
 `;
 
-const NavLink = styled.a`
-`
+const NavLink = styled.a``;
 
-const DashboardNavbar = () => 
-
-    <NavContainer>
-      <NavLinks>
-        <NavItem><NavLink href='#'>Dashboard</NavLink></NavItem>
-        <NavItem><NavLink href='#'>Projects</NavLink></NavItem>
-        <NavItem><NavLink href='#'>Add</NavLink></NavItem>
-      </NavLinks>
-      <NavLinks>
-        <NavItem><NavLink href='#'>Notifications</NavLink></NavItem>
-        <NavItem><NavLink href='#'>Search</NavLink></NavItem>
-        <NavItem><NavLink href='#'>Profile</NavLink></NavItem>
-      </NavLinks>
-    </NavContainer>
+const DashboardNavbar = () => (
+  <NavContainer>
+    <NavLinks>
+      <NavItem>
+        <NavLink href="#">Dashboard</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Projects</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Add</NavLink>
+      </NavItem>
+    </NavLinks>
+    <NavLinks>
+      <NavItem>
+        <NavLink href="#">Notifications</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Search</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Profile</NavLink>
+      </NavItem>
+    </NavLinks>
+  </NavContainer>
+);
 
 export default DashboardNavbar;
