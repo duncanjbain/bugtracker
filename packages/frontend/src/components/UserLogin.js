d3a5059708430611b55fdff7d44735d18ce66cca
Duncan Bain <duncan@bain.io>
2021-02-25 19:34:15 +0000

    improve accessibility, change form input style on error

--- packages/frontend/src/components/UserLogin.js
+++ packages/frontend/src/components/UserLogin.js
@@ -30,33 +30,49 @@ const UserLogin = () => {
 
   return (
     <SignupFormContainer>
-      <h2>Log In</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          <InputLabel htmlFor="username">Username</InputLabel>
-          <TextInput
-            id="Username"
-            type="text"
-            placeholder="Username"
-            name="username"
-            ref={register({ required: true })}
-          />
-          {errors.username && <p>{errors.username.message}</p>}
-        </FormGroup>
-        <FormGroup>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <TextInput
-            id="password"
-            type="text"
-            placeholder="Password"
-            name="password"
-            ref={register({ required: true })}
-          />
-          {errors.password && <p>{errors.password.message}</p>}
-        </FormGroup>
+      <main style={{ padding: '1rem' }}>
+        <h2>Log In</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup>
+            <InputLabel htmlFor="username">Username</InputLabel>
+            <TextInput
+              id="username"
+              type="text"
+              placeholder="Username"
+              name="username"
+              ref={register({ required: true })}
+              aria-required="true"
+              aria-invalid={errors.username ? 'true' : 'false'}
+              className={errors.username ? 'error' : ''}
+            />
+            {errors.username && (
+              <p id="username-error" role="alert">
+                {errors.username.message}
+              </p>
+            )}
+          </FormGroup>
+          <FormGroup>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <TextInput
+              id="password"
+              type="password"
+              placeholder="Password"
+              name="password"
+              ref={register({ required: true })}
+              aria-required="true"
+              aria-invalid={errors.password ? 'true' : 'false'}
+              className={errors.password ? 'error' : ''}
+            />
+            {errors.password && (
+              <p id="password-error" role="alert">
+                {errors.password.message}
+              </p>
+            )}
+          </FormGroup>
 
-        <SubmitButton type="submit">Log In</SubmitButton>
-      </form>
+          <SubmitButton type="submit">Log In</SubmitButton>
+        </form>
+      </main>
     </SignupFormContainer>
   );
 };
--- packages/frontend/src/ui/components/StyledForm.js
+++ packages/frontend/src/ui/components/StyledForm.js
@@ -5,7 +5,6 @@ export const SubmitButton = styled.button`
 `;
 
 export const FormGroup = styled.div`
-  padding: 0.5rem;
   justify-content: space-between;
   display: flex;
   flex-direction: column;
@@ -19,10 +18,27 @@ export const SignupFormContainer = styled.div`
 `;
 
 export const InputLabel = styled.label`
+  font-size: 1.5rem;
+  font-weight: 400;
   padding-right: 0.5rem;
+  margin-bottom: 0.5rem;
 `;
 
 export const TextInput = styled.input`
   padding: 0.5rem;
   width: 100%;
+  border: 2px solid ${(props) => props.theme.colors.dark};
+  border-radius: 4px;
+  line-height: 1;
+  transition: 180ms box-shadow ease-in-out;
+  &:focus {
+    border-color: ${(props) => props.theme.colors.primary};
+    box-shadow: 0 0 0 3px hsla(171, 100%, 41%, 0.8);
+  }
+  &.error {
+    border: 2px solid ${(props) => props.theme.colors.danger};
+    &:focus {
+      box-shadow: 0 0 0 3px hsla(348, 100%, 61%, 0.8);
+    }
+  }
 `;
